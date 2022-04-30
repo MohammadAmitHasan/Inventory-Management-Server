3f55dc1a1c380e559a3728c13687a77ad2f3d72a
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-04-30 14:41:28 +0600

    Mongo Connected and product API implemented

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const cors = require('cors')
+
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 // Using middleware
 app.use(cors())
 app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mhdxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('nim_database').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+    }
+    finally { }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Nasah Inventory Management Server is Running')
