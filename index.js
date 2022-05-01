a280045fbcb9e7a7964e08e95d992244d5e7af0c
Muhammad Amit Hasan <bhamithasan@gmail.com>
2022-05-01 15:29:50 +0600

    Update product stock implemented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express()
 require('dotenv').config()
@@ -10,20 +11,56 @@ app.use(cors())
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const query = require('express/lib/middleware/query');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mhdxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
+        // Connect To database
         await client.connect();
         const productCollection = client.db('nim_database').collection('products');
 
+        // Create all products API
         app.get('/products', async (req, res) => {
+            const amount = req.query.amount;
             const query = {};
-            const cursor = productCollection.find(query);
+            let cursor;
+            if (amount) {
+                cursor = productCollection.find(query).limit(+amount);
+            }
+            else {
+                cursor = productCollection.find(query);
+            }
+
             const products = await cursor.toArray();
             res.send(products);
         })
+
+        // Create specific product API
+        app.get('/products/:id', async (req, res) => {
+            const productId = req.params.id;
+            const query = { _id: ObjectId(productId) }
+            const result = await productCollection.findOne(query);
+            res.send(result);
+        })
+
+        app.put('/updateStock/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+
+            const newAmount = req.body;
+            console.log(newAmount)
+
+            const updateDoc = {
+                $set: newAmount
+            }
+
+            const result = await productCollection.updateOne(filter, updateDoc, options)
+            res.send(result);
+        })
+
     }
     finally { }
 }
